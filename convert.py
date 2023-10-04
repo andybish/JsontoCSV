89881c598ad7133396ee94887bd897c1c2359c5b
Andrew Bishop <andybish@users.noreply.github.com>
2023-10-04 13:49:03 +0100

    Create convert.py

---
+++ convert.py
@@ -0,0 +1,35 @@
+headers = ['id64', 'name', 'coords', 'date']
+filename = 'systemsWithCoordinates'
+
+import json
+import csv
+
+file = open(filename + '.json')
+j = json.load(file)
+file.close()
+c = open(filename + '.csv', 'w')
+writer = csv.writer(c)
+
+writer.writerow(headers)
+count = 0
+missings = {}
+for row in j:
+    data = []
+    for h in headers:
+        if h in row:
+            data.append(row[h])
+        else:
+            if h not in missings:
+                missings[h]=1
+            else:
+                missings[h]+=1
+            data.append('')
+    try:
+        writer.writerow(data)
+    except:
+        print(row)
+        break
+    count+=1
+print(count)
+print(missings)
+c.close()
