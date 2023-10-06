f36efef88280d82d4ac9b0d63b66cc8b7b2770a3
Andrew Bishop <andybish@users.noreply.github.com>
2023-10-06 18:02:18 +0100

    Update convert.py
    Better handling for questionable data quality. Now prints number of missing fields, number of present but null/empty fields, and number skipped due to missing or empty primary key.

--- convert.py
+++ convert.py
@@ -1,3 +1,5 @@
+#star headers = ['id64', 'bodyId', 'name', 'type', 'subType', 'parents', 'distanceToArrival', 'isMainStar', 'isScoopable', 'age', 'luminosity', 'absoluteMagnitude', 'solarMasses', 'solarRadius', 'surfaceTemperature', 'orbitalPeriod', 'semiMajorAxis', 'orbitalEccentricity', 'orbitalInclination', 'argOfPeriapsis', 'rotationalPeriod', 'rotationalPeriodTidallyLocked', 'axialTilt', 'belts', 'updateTime', 'systemId64', 'systemName']
+#planet headers = ['id64', 'bodyId', 'name', 'type', 'subType', 'parents', 'distanceToArrival', 'isLandable', 'gravity', 'earthMasses', 'radius', 'surfaceTemperature', 'surfacePressure', 'volcanismType', 'atmosphereType', 'atmosphereComposition', 'solidComposition', 'terraformingState', 'orbitalPeriod', 'semiMajorAxis', 'orbitalEccentricity', 'orbitalInclination', 'argOfPeriapsis', 'rotationalPeriod', 'rotationalPeriodTidallyLocked', 'axialTilt', 'rings', 'materials', 'updateTime', 'systemId64', 'systemName']
 headers = ['id64', 'name', 'coords', 'date']
 filename = 'systemsWithCoordinates'
 
@@ -13,20 +15,39 @@ writer = csv.writer(c, dialect='unix', escapechar='\\', doublequote=False)
 writer.writerow(headers)
 count = 0
 missings = {}
+nones = {}
+skipped = 0
 for row in j:
+    skip = False
     data = []
+#    if row['type'] != 'Star':
     for h in headers:
+        if skip == True:
+            continue
         if h in row:
-            if '{' in str(row[h]):
-                data.append(json.dumps(row[h]))
+            if row[h] is None:
+                if h not in nones:
+                    nones[h]=1
+                else:
+                    nones[h]+=1
+                if h != 'id64':
+                    data.append('')
+                else:
+                    skipped += 1
+                    skip = True
+                    continue
+            elif '{' in str(row[h]) or '[' in str(row[h]):
+                data.append(json.dumps(row[h]).replace('�','ñ')) #fuck fdev
             else:
-                data.append(row[h])
+                data.append(str(row[h]).replace('�','ñ')) #no really, fuck fdev
         else:
             if h not in missings:
                 missings[h]=1
             else:
                 missings[h]+=1
             data.append('')
+    if skip == True:
+        continue
     try:
         writer.writerow(data)
     except:
@@ -35,4 +56,6 @@ for row in j:
     count+=1
 print(count)
 print(missings)
+print(nones)
+print(skipped)
 c.close()
